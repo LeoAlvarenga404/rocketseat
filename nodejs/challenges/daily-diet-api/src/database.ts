932d54abb62a1a844f09dbb06db90ea490c848d8
Leonardo <leonardo.palvarenga@gmail.com>
2025-08-31 18:59:00 -0300

    initial setup: daily diet API

---
+++ nodejs/challenges/daily-diet-api/src/database.ts
@@ -0,0 +1,20 @@
+import setupKnex, { type Knex } from "knex";
+import { env } from "./env/";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL não configurada.");
+}
+
+export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+};
+
+export const knex = setupKnex(config);
